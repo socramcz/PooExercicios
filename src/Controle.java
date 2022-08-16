8a8a3a109b4ba355cb8a9061c83734af233dcdfc
socramcz <josemarcosf12@gmail.com>
2022-08-16 20:12:54 -0300

    exercise commit

--- src/Controle.java
+++ src/Controle.java
@@ -34,6 +34,14 @@ public class Controle {
         System.out.println("\n>>> Volume aumentado para "+ test1.volume);
         test1.diminuirVolume();
         System.out.println(">>> Volume abaixado para "+ test1.volume);
+
+        // FORMA DO "FOR" NO JAVA
+        // "i++" É A MESMA COISA DE i+=1
+        for (int i = 0; i< 10; i++){
+            test1.aumentarVolume();
+            System.out.println(">>> Volume aumentado para "+ test1.volume);
+        }
+
         test1.diminuirVolume();
         System.out.println(">>> Volume abaixado para "+ test1.volume);
         System.out.println("\n>>> Canal trocado para "+ test1.setCanal(70));
