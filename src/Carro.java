958543c98e079fe1f2dab81334989cd57b016ce6
socramcz <josemarcosf12@gmail.com>
2022-08-22 21:40:40 -0300

    exercise commit

---
+++ src/Carro.java
@@ -0,0 +1,36 @@
+// Crie uma classe carro com os atributos
+// combustivel = 0 e consumo (km/litro)
+// Metodos serão:
+// Andar(double distancia) > Reduz a quantidade de combustivel de acordo com a distancia
+// Abastecer(double quantidade) > aumenta o combustivel
+
+public class Carro {
+    private double consumo;
+    private double combustivel;
+
+    public Carro(double consumo){
+        this.consumo = consumo;
+    }
+
+    private void andar(double distancia){
+        double gasto = distancia/this.consumo;
+        if(gasto <= combustivel) {
+            this.combustivel -= test;
+            System.out.println("\n=> Carro percorreu " + distancia + "KM" + "\nGastou: " + gasto + "L" + "\nCombustivel restante: " + combustivel);
+        }
+        else{
+            System.out.println("Combustivel insuficiente");
+        }
+    }
+
+    private void abastecer(double quantidade){
+        combustivel += quantidade;
+        System.out.println("\n=> Carro abastecido com " +quantidade+ "L"+"\nTanque: " +combustivel+ "L");
+    }
+
+    public static void main(String[] args) {
+        Carro corsa = new Carro(25);
+        corsa.abastecer(30);
+        corsa.andar(120);
+    }
+}
