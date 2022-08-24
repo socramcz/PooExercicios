118021713df287328204cabb264ba8da71958001
socramcz <josemarcosf12@gmail.com>
2022-08-24 00:10:00 -0300

    Exercises

---
+++ src/Pessoa.java
@@ -0,0 +1,50 @@
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private double peso;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double peso, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public void envelhecer(int anos){
+        this.idade += anos;
+        if(this.idade <= 21){
+            crescer(0.5f);
+        }
+    }
+
+    public void engordar(double novoPeso){
+        this.peso += novoPeso;
+    }
+
+    public void emagrecer(double novoPeso){
+        this.peso -= novoPeso;
+    }
+
+    public void crescer(double novaAltura){
+        this.altura = novaAltura;
+    }
+
+    public String toString(){
+        return "Pessoa [nome=" + nome + ", idade=" + idade + ", peso=" + peso + ", altura=" + altura + "]";
+    }
+
+    public static void main(String[] args) {
+        Pessoa a = new Pessoa("marcos", 19, 83, 177);
+        System.out.println(a.toString());
+        a.engordar(5);
+        System.out.println(a.toString());
+        a.emagrecer(10);
+        System.out.println(a.toString());
+        a.crescer(3);
+        System.out.println(a.toString());
+        a.envelhecer(1);
+        System.out.println(a.toString());
+    }
+}
